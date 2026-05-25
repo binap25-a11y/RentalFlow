e3cb0d45493a02d9fedce11c3b1c523d734eff30
B P <Binap25@gmail.com>
2026-05-25 20:58:10 +0000

    revert back to last stable state of the application

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
 }
 
 /**
